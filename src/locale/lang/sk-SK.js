2427122f76d0763a470e3dd75d8a9e7f4b88297b
belusky <beliansky.michal@gmail.com>
2018-10-10 20:45:16 +0200

    sk-SK locale added

---
+++ src/locale/lang/sk-SK.js
@@ -0,0 +1,41 @@
+export default {
+  uiv: {
+    datePicker: {
+      clear: 'Zrušiť',
+      today: 'Dnes',
+      month: 'Mesiac',
+      month1: 'Január',
+      month2: 'Február',
+      month3: 'Marec',
+      month4: 'Apríl',
+      month5: 'Máj',
+      month6: 'Jún',
+      month7: 'Júl',
+      month8: 'August',
+      month9: 'September',
+      month10: 'Október',
+      month11: 'November',
+      month12: 'December',
+      year: 'Rok',
+      week1: 'Po',
+      week2: 'Ut',
+      week3: 'St',
+      week4: 'Št',
+      week5: 'Pi',
+      week6: 'So',
+      week7: 'Ne'
+    },
+    timePicker: {
+      am: 'AM',
+      pm: 'PM'
+    },
+    modal: {
+      cancel: 'Zrušiť',
+      ok: 'OK'
+    },
+    multiSelect: {
+      placeholder: 'Vybrať...',
+      filterPlaceholder: 'Hľadať...'
+    }
+  }
+}
